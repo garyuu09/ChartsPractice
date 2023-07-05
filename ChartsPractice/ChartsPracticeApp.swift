73b52a83af451a6e4150073a67d338fd61364e65
Ryuga <ribereo.minami@gmail.com>
2023-07-05 16:56:41 +0900

    Initial Commit

---
+++ ChartsPractice/ChartsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsPracticeApp.swift
+//  ChartsPractice
+//
+//  Created by Ryuga on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct ChartsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
